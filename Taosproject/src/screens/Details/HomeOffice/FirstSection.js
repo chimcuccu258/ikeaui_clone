855149ecfffd8b9c349f06a2bb63e8d2dd9679e7
chimcuccu258 <vuminhnga25@gmail.com>
2023-08-03 19:13:34 +0700

    update 0308_2

--- Taosproject/src/screens/Details/HomeOffice/FirstSection.js
+++ Taosproject/src/screens/Details/HomeOffice/FirstSection.js
@@ -9,6 +9,7 @@ import {
 import MasonryList from '@react-native-seoul/masonry-list';
 import React, {useRef} from 'react';
 import {COLORS} from '../../../database/database';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
 
 const FirstSection = () => {
   const DATA = [
@@ -32,45 +33,57 @@ const FirstSection = () => {
 
   return (
     <View style={{marginTop: 40}}>
-      <Text style={{fontWeight: '700', fontSize: 18}}>
-        Working from home: we make it work for you
-      </Text>
-      <Text style={{fontSize: 14, marginTop: 10, letterSpacing: 0.3}}>
-        It is important to have a nice place to work at home. We can make sure
-        that you will have everything you need to create a comfortable workspace
-        at home. From a good desk to a comfortable office chair and from a handy
-        cabinet to the right lighting for the dark days to come.
-      </Text>
-      <Image
-        source={require('../../../database/images/rooms/HomeOffice/FirstSection/FirstSection_1.jpg')}
-        style={{width: '100%', height: 400, marginTop: 20}}
-      />
+      <View>
+        <Text style={{fontWeight: '700', fontSize: 18}}>
+          Working from home: we make it work for you
+        </Text>
+        <Text style={{fontSize: 14, marginTop: 10, letterSpacing: 0.3}}>
+          It is important to have a nice place to work at home. We can make sure
+          that you will have everything you need to create a comfortable
+          workspace at home. From a good desk to a comfortable office chair and
+          from a handy cabinet to the right lighting for the dark days to come.
+        </Text>
+      </View>
 
-      <FlatList
-        data={DATA}
-        numColumns={2}
-        keyExtractor={item => item.id}
-        renderItem={({item}) => {
-          return (
-            <TouchableOpacity activeOpacity={0.5} style={{marginTop: 10}}>
-              <View
-                style={{
-                  display: 'flex',
-                }}>
-                <Image
+      <View style={{marginTop: 20,}}>
+        <TouchableOpacity activeOpacity={0.5}>
+          <Image
+            source={require('../../../database/images/rooms/HomeOffice/FirstSection/FirstSection_1.jpg')}
+            style={{width: '100%', height: 400, marginTop: 20, zIndex: 0}}
+          />
+        </TouchableOpacity>
+        <FlatList
+          data={DATA}
+          numColumns={2}
+          keyExtractor={item => item.id}
+          renderItem={({item}) => {
+            return (
+              <TouchableOpacity activeOpacity={0.5} style={{marginTop: 10}}>
+                <View
                   style={{
-                    objectFit: 'cover',
-                    width: Dimensions.get('window').width / 2 - 10,
-                    height: 200,
-                    marginRight: 10,
-                  }}
-                  source={item.image}
-                />
-              </View>
-            </TouchableOpacity>
-          );
-        }}
-      />
+                    display: 'flex',
+                  }}>
+                  <Image
+                    style={{
+                      objectFit: 'cover',
+                      width: Dimensions.get('window').width / 2 - 10,
+                      height: 200,
+                      marginRight: 10,
+                    }}
+                    source={item.image}
+                  />
+                </View>
+              </TouchableOpacity>
+            );
+          }}
+        />
+        
+      </View>
+      <View style={{zIndex: 1}}>
+          <Text style={{position: 'absolute', zIndex: 10, color: COLORS.blue}}>
+            hihi
+          </Text>
+        </View>
     </View>
   );
 };
